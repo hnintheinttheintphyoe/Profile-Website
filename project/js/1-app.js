f7345a791b742706b22cb90de1fd0abf5e1354af
Hnin Theint Theint Phyo <hnintheinttheintphyoe@gmail.com>
2022-03-26 13:37:36 +0630

    first commit

---
+++ project/js/1-app.js
@@ -0,0 +1,14 @@
+
+    let screenHeight=$(window).height();
+    console.log(screenHeight);
+    $(window).scroll(function(){
+        let currentPosition=$(this).scrollTop();
+        console.log(currentPosition);
+        if(currentPosition > screenHeight-100){
+            $(".site-nav").addClass("site-nav-scroll");
+        }
+        else{
+            $(".site-nav").removeClass("site-nav-scroll");
+        }
+    });
+
